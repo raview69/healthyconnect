39cd5870e48b441db4d8abfbf1e6b4ab51c3f8fc
Ravi <raview69@coding.local>
2024-05-20 23:48:58 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Dashboard from "./components/dashboard/Dashboard";
 import TodoDetails from "./components/todo/TodoDetails";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="quests/:id/edit" element={<TodoDetails />} />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="quests/:id/edit" element={<TodoDetails />} /> */}
       </Routes>
     </>
   );
